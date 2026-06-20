502aaa83aed0eddb4b9e4b42a3a6a0360dae7deb
Akbar <Akbar160296@gmail.com>
2026-06-20 11:14:55 +0800

    add category config system

---
+++ config/categoryConfig.js
@@ -0,0 +1,22 @@
+const categoryConfig = [
+
+    {
+        path: "/flashsale",
+        componentId: "464113167266a3241aa7cdf37.67370314"
+    },
+
+    {
+        path: "/fashion",
+        componentId: "282513168266a338f586bef13.52829636",
+        title: "Fashion Terbaru"
+    },
+
+    {
+        path: "/kecantikan",
+        componentId: "562513168266a338f74385b73.47876782",
+        title: "Produk Kecantikan"
+    }
+
+];
+
+module.exports = categoryConfig;
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,25 +1,18 @@
 const createCategoryPage = require("../controllers/categoryController");
 
+const categoryConfig = require("../config/categoryConfig");
+
 module.exports = function (app) {
 
-    createCategoryPage(
-        app,
-        "/flashsale",
-        "464113167266a3241aa7cdf37.67370314"
-    );
+    categoryConfig.forEach(category => {
 
-    createCategoryPage(
-        app,
-        "/fashion",
-        "282513168266a338f586bef13.52829636",
-        "Fashion Terbaru"
-    );
+        createCategoryPage(
+            app,
+            category.path,
+            category.componentId,
+            category.title
+        );
 
-    createCategoryPage(
-        app,
-        "/kecantikan",
-        "562513168266a338f74385b73.47876782",
-        "Produk Kecantikan"
-    );
+    });
 
 };
