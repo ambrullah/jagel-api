eab008c361483e473072e876fe2765c288303628
Akbar <Akbar160296@gmail.com>
2026-06-20 13:51:23 +0800

    update

--- config/bannerConfig.js
+++ config/bannerConfig.js
@@ -7,15 +7,15 @@ const bannerConfig = {
         {
             image: "https://res.cloudinary.com/dutuhbbg2/image/upload/v1781340974/ChatGPT_Image_13_Jun_2026_16.55.58_vyhqxr.png",
             link: "promo-detail",
-            start: "2026-06-09 02:06:00",
-            end: "2026-12-09 03:55:10"
+            start: "2025-06-09 02:06:00",
+            end: "2025-12-09 03:55:10"
         },
 
         {
             image: "https://res.cloudinary.com/dutuhbbg2/image/upload/v1781341128/ChatGPT_Image_13_Jun_2026_16.58.33_cnhr35.png",
             link: "",
-            start: "2026-06-09 02:24:00",
-            end: "2026-12-09 15:55:10"
+            start: "2025-06-09 02:24:00",
+            end: "2025-12-09 15:55:10"
         }
 
     ]
