2728b2296ab3fcb7b38c12e517d158210d635548
Akbar <Akbar160296@gmail.com>
2026-06-20 13:37:50 +0800

    perbarui benner

--- config/bannerOngkirConfig.js
+++ config/bannerOngkirConfig.js
@@ -5,14 +5,14 @@ const bannerOngkirConfig = {
     items: [
 
         {
-            image: "https://......png",
+            image: "https://res.cloudinary.com/dutuhbbg2/image/upload/v1781336237/file_00000000a1287208855b0758be08f50a_plh59q.png",
             link: "",
             start: "2026-06-20 00:00:00",
             end: "2026-12-31 23:59:59"
         },
 
         {
-            image: "https://......png",
+            image: "https://res.cloudinary.com/dutuhbbg2/image/upload/v1781336237/file_00000000a1287208855b0758be08f50a_plh59q.png",
             link: "",
             start: "2026-06-20 00:00:00",
             end: "2026-12-31 23:59:59"
