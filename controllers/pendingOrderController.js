67a2b0b17fc7badd83252d8904b0618a06337f94
Akbar <Akbar160296@gmail.com>
2026-06-22 12:20:34 +0800

    update fitur baru

--- controllers/pendingOrderController.js
+++ controllers/pendingOrderController.js
@@ -1,64 +1,9 @@
-const axios = require("axios");
-const pendingOrderTemplate =
-    require("../views/pendingOrder/pendingOrderTemplate");
-
-async function pendingOrderController(req, res) {
-
-    try {
-
-        const response =
-            await axios.post(
-                "https://api.jagel.id/v1/pendingOrder",
-                {
-                    apikey:
-                        process.env.JAGEL_API_KEY
-                },
-                {
-                    headers: {
-                        Accept:
-                            "application/json"
-                    }
-                }
-            );
-
-        const orders =
-            response.data.data || [];
-
-        res.send(
-            pendingOrderTemplate(
-                orders
-            )
-        );
-
+const response = await axios.get(
+    "https://api.jagel.id/v1/pendingOrder",
+    {
+        headers: {
+            Accept: "application/json",
+            apikey: process.env.JAGEL_API_KEY
+        }
     }
-    catch (error) {
-
-        console.log(
-            error.response?.data
-        );
-
-        res.send(`
-<h3>
-Gagal mengambil data pending order
-</h3>
-
-<pre>
-${error.message}
-
----------------------
-
-${JSON.stringify(
-            error.response?.data,
-            null,
-            4
-        )}
-
-</pre>
-`);
-
-    }
-
-}
-
-module.exports =
-    pendingOrderController;
+);
