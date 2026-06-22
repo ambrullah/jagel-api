4503c584da4b22ecea46afc4e363c7bd28578291
Akbar <Akbar160296@gmail.com>
2026-06-22 18:50:54 +0800

    update data api

--- routes/topupApiRoute.js
+++ routes/topupApiRoute.js
@@ -3,6 +3,18 @@ const topupApiController =
 
 module.exports = function (app) {
 
+    app.get(
+        "/api/topup",
+        (req, res) => {
+
+            res.json({
+                success: true,
+                message: "Topup API aktif"
+            });
+
+        }
+    );
+
     app.post(
         "/api/topup",
         topupApiController
